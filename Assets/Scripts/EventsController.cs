3aedf9284c552bf7ac9af19ce1a8e15292f4dcb9
BurnEmDown <amitreuveni@gmail.com>
2022-08-21 19:09:37 +0300

    Events shown to player

--- Assets/Scripts/EventsController.cs
+++ Assets/Scripts/EventsController.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EventsController : MonoBehaviour
 {
     public static EventsController Instance;
 
-    private List<Event> quarterEventsList;
-    private List<Event> newYearEventsList;
+    [SerializeField] private List<Event> quarterEventsList;
+    [SerializeField] private List<Event> newYearEventsList;
 
     void Awake()
     {
@@ -21,20 +22,27 @@ public class EventsController : MonoBehaviour
         }
     }
 
+    public void OnGUI()
+    {
+        ShowNewEvent(null);
+    }
+
     public void ShowNewEvent(Action onComplete)
     {
+        int eventNum = Random.Range(0, quarterEventsList.Count-1);
+
+        Event quarterEvent = quarterEventsList[eventNum];
+        EventsView.Instance.ShowEvent(quarterEvent);
+        DisableInvalidEventOptions(quarterEvent);
         
-        
-        
-        onComplete.Invoke();
+        onComplete?.Invoke();
     }
 
     public void ShowNewYearEvent(Action onComplete)
     {
         
         
-        
-        onComplete.Invoke();
+        onComplete?.Invoke();
     }
     
     private void DisableInvalidEventOptions(Event eventToCheck)
--- Assets/Scripts/EventsView.cs
+++ Assets/Scripts/EventsView.cs
@@ -1,17 +1,23 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EventsView : MonoBehaviour
 {
     public static EventsView Instance;
 
     public GameObject eventTextBox;
-    public TMP_Text eventText;
+    public TMP_Text eventDescriptionText;
+    public TMP_Text eventNameText;
     public GameObject eventOptionsBox;
-    public GameObject eventOptionA;
-    public GameObject eventOptionB;
-    public GameObject eventOptionC;
-    public GameObject eventOptionD;
+    public Button eventOptionA;
+    public TMP_Text eventOptionAText;
+    public Button eventOptionB;
+    public TMP_Text eventOptionBText;
+    public Button eventOptionC;
+    public TMP_Text eventOptionCText;
+    public Button eventOptionD;
+    public TMP_Text eventOptionDText;
     
     void Awake()
     {
@@ -28,18 +34,55 @@ public class EventsView : MonoBehaviour
     public void HideAllEventUI()
     {
         eventTextBox.SetActive(false);
-        eventText.gameObject.SetActive(false);
+        eventDescriptionText.gameObject.SetActive(false);
+        eventNameText.gameObject.SetActive(false);
         eventOptionsBox.SetActive(false);
-        eventOptionA.SetActive(false);
-        eventOptionB.SetActive(false);
-        eventOptionC.SetActive(false);
-        eventOptionD.SetActive(false);
+        eventOptionA.gameObject.SetActive(false);
+        eventOptionB.gameObject.SetActive(false);
+        eventOptionC.gameObject.SetActive(false);
+        eventOptionD.gameObject.SetActive(false);
+        eventOptionAText.gameObject.SetActive(false);
+        eventOptionBText.gameObject.SetActive(false);
+        eventOptionCText.gameObject.SetActive(false);
+        eventOptionDText.gameObject.SetActive(false);
+    }
+
+    public void UpdateEventTextBox(Event eventToShow)
+    {
+        eventDescriptionText.text = eventToShow.eventText;
+        eventNameText.text = eventToShow.eventName;
+    }
+
+    public void UpdateEventOptions(Event eventToShow)
+    {
+        switch (eventToShow.eventOptions.Count)
+        {
+            case 1:
+                eventOptionAText.text = eventToShow.eventOptions[0].optionText;
+                break;
+            case 2:
+                eventOptionAText.text = eventToShow.eventOptions[0].optionText;
+                eventOptionBText.text = eventToShow.eventOptions[1].optionText;
+                break;
+            case 3:
+                eventOptionAText.text = eventToShow.eventOptions[0].optionText;
+                eventOptionBText.text = eventToShow.eventOptions[1].optionText;
+                eventOptionCText.text = eventToShow.eventOptions[2].optionText;
+                break;
+            case 4:
+                eventOptionAText.text = eventToShow.eventOptions[0].optionText;
+                eventOptionBText.text = eventToShow.eventOptions[1].optionText;
+                eventOptionCText.text = eventToShow.eventOptions[2].optionText;
+                eventOptionDText.text = eventToShow.eventOptions[3].optionText;
+                break;
+        }
     }
 
     public void ShowEventBoxAndText()
     {
         eventTextBox.SetActive(true);
-        eventText.gameObject.SetActive(true);
+        eventDescriptionText.gameObject.SetActive(true);
+        eventNameText.gameObject.SetActive(true);
     }
 
     public void ShowEventOptions(Event eventToShow)
@@ -49,23 +92,52 @@ public class EventsView : MonoBehaviour
         switch (eventToShow.eventOptions.Count)
         {
             case 1:
-                eventOptionA.SetActive(true);
+                eventOptionA.gameObject.SetActive(true);
+                eventOptionAText.gameObject.SetActive(true);
+                eventToShow.eventOptions[0].assignedButton = eventOptionA;
                 break;
             case 2:
-                eventOptionA.SetActive(true);
-                eventOptionB.SetActive(true);
+                eventOptionA.gameObject.SetActive(true);
+                eventOptionAText.gameObject.SetActive(true);
+                eventToShow.eventOptions[0].assignedButton = eventOptionA;
+                eventOptionB.gameObject.SetActive(true);
+                eventOptionBText.gameObject.SetActive(true);
+                eventToShow.eventOptions[1].assignedButton = eventOptionB;
                 break;
             case 3:
-                eventOptionA.SetActive(true);
-                eventOptionB.SetActive(true);
-                eventOptionC.SetActive(true);
+                eventOptionA.gameObject.SetActive(true);
+                eventOptionAText.gameObject.SetActive(true);
+                eventToShow.eventOptions[0].assignedButton = eventOptionA;
+                eventOptionB.gameObject.SetActive(true);
+                eventOptionBText.gameObject.SetActive(true);
+                eventToShow.eventOptions[1].assignedButton = eventOptionB;
+                eventOptionC.gameObject.SetActive(true);
+                eventOptionCText.gameObject.SetActive(true);
+                eventToShow.eventOptions[2].assignedButton = eventOptionC;
                 break;
             case 4:
-                eventOptionA.SetActive(true);
-                eventOptionB.SetActive(true);
-                eventOptionC.SetActive(true);
-                eventOptionD.SetActive(true);
+                eventOptionA.gameObject.SetActive(true);
+                eventOptionAText.gameObject.SetActive(true);
+                eventToShow.eventOptions[0].assignedButton = eventOptionA;
+                eventOptionB.gameObject.SetActive(true);
+                eventOptionBText.gameObject.SetActive(true);
+                eventToShow.eventOptions[1].assignedButton = eventOptionB;
+                eventOptionC.gameObject.SetActive(true);
+                eventOptionCText.gameObject.SetActive(true);
+                eventToShow.eventOptions[2].assignedButton = eventOptionC;
+                eventOptionD.gameObject.SetActive(true);
+                eventOptionDText.gameObject.SetActive(true);
+                eventToShow.eventOptions[3].assignedButton = eventOptionD;
                 break;
         }
+    }
+
+    public void ShowEvent(Event quarterEvent)
+    {
+        HideAllEventUI();
+        ShowEventBoxAndText();
+        UpdateEventTextBox(quarterEvent);
+        ShowEventOptions(quarterEvent);
+        UpdateEventOptions(quarterEvent);
     }
 }
