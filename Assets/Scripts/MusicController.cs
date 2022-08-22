9802d9f3e68f8cd6a4045dfd554641902a78428b
BurnEmDown <amitreuveni@gmail.com>
2022-08-22 14:05:53 +0300

    Added music and music controller

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public static MusicController Instance;
+
+    public AudioSource musicSource;
+    
+    void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+    
+    
+}
